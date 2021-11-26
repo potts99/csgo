c277099ed6857e35e04d14656d3cb5516378faac
Jack Andrews <japotts16@gmail.com>
2021-11-26 00:40:53 +0000

    working b01 basic

--- pages/game.js
+++ pages/game.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useQuery } from "react-query";
-import _, { includes } from "lodash";
+import { indexOf, remove } from "lodash";
 
 export default function game() {
   async function game() {
@@ -25,9 +25,11 @@ export default function game() {
 
   const { data, status } = useQuery("game", game);
 
-  async function playfull() {
-    let m_team = data.m_t.players;
-    let o_team = data.o_t.players;
+  async function playRound() {
+    const m_team = [...data.m_t.players];
+    const o_team = [...data.o_t.players];
+
+    console.log(m_team)
 
     if (a > 15 || b > 15) {
       console.log("Game over");
@@ -74,30 +76,89 @@ export default function game() {
         element.stats = { ...stats, weight: Math.round(total) };
       });
 
-      let a_player = m_team[Math.floor(Math.random() * m_team.length)];
-      let b_player = o_team[Math.floor(Math.random() * o_team.length)];
-      let hat = [];
+      // Create a loop
+      // Pick two players
+      // put both weights in array hat
+      // pick at random
+      // winner gets a kill
+      // loser gets a death and removed from array
+      // Team to lose all players first loses round
+      // winning team + 1 score
+      // Add round details too round array
 
-      for (let i = 0; i < a_player.stats.weight; i++) {
-        hat.push(a_player.ign);
+      let active = true;
+
+      while (active === true) {
+
+        const a_team = m_team
+        const b_team = o_team
+        
+        console.log(a_team.length, b_team.length);
+
+        if (a_team.length === 0 || b_team.length === 0) {
+          console.log("round over amingo");
+
+          if(b_team.length === 0) {
+              console.log('you win')
+              setA(a + 1)
+          } else {
+            console.log('you lose')
+            setB(b + 1)
+          }
+
+        break
+        } else {
+          let a_player = a_team[Math.floor(Math.random() * a_team.length)];
+          let b_player = b_team[Math.floor(Math.random() * b_team.length)];
+
+          let hat = [];
+
+          for (let i = 0; i < a_player.stats.weight; i++) {
+            hat.push(a_player.ign);
+          }
+
+          for (let i = 0; i < b_player.stats.weight; i++) {
+            hat.push(b_player.ign);
+          }
+
+          const winner = hat[Math.floor(Math.random() * hat.length)];
+
+          function findloser() {
+            let h = new Set(hat);
+            let it = h.values();
+            let arr = Array.from(it);
+            return arr.filter(function (value, index, arr) {
+              return value !== winner;
+            });
+          }
+
+          const checkwinner = (obj) => obj.ign === winner;
+
+          if (a_team.some(checkwinner)) {
+            const loser = findloser();
+
+            remove(b_team, function (n) {
+              return n.ign === String(loser);
+            });
+
+            let k = b_team.length
+            k--
+
+            console.log("astralis killed " + String(loser) + " with " + winner);
+          } else {
+            const loser = findloser();
+
+            remove(a_team, function (n) {
+              return n.ign === String(loser);
+            });
+
+            let k = a_team.length
+            k--
+
+            console.log("liquid killed " + String(loser) + " with " + winner);
+          }
+        }
       }
-
-      for (let i = 0; i < b_player.stats.weight; i++) {
-        hat.push(b_player.ign);
-      }
-
-      const winner = hat[Math.floor(Math.random() * hat.length)]
-
-      const checkwinner = obj => obj.ign === winner;
-
-      if(m_team.some(checkwinner)) {
-          console.log('astralis wins with ' + winner)
-          setA(a + 1)
-      } else {
-        console.log('liquid wins with ' + winner)
-        setB(b + 1)
-      }
-
     }
   }
 
@@ -132,7 +193,12 @@ export default function game() {
               <div>
                 astralis {a} - {b} liquid
               </div>
-              <button disabled={live === false ? true : false} onClick={() => playfull()}>play round</button>
+              <button
+                disabled={live === false ? true : false}
+                onClick={() => playRound()}
+              >
+                play round
+              </button>
             </div>
 
             <div className="">
